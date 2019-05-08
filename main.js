0e6f9b07156125f186dd8e9a4a44f3fd57590d40
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2019-05-09 11:55:56 +1200

    Css complete, flashing background, scrolling to second page.

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+// enables the down link to scroll from he main page to the second page.
 $(function() {
-    $('.scroll-down').click (function() {
+    $('.down').click (function() {
       $('html, body').animate({scrollTop: $('section.second-page').offset().top }, 'slow');
       return false;
     });
   });
 
+  // enables the up link/button to scroll from second page to the main landing page.
   $(function() {
-    $('.scroll-up').click (function() {
+    $('.up').click (function() {
       $('html, body').animate({scrollTop: $('section.landing-page').offset().top }, 'slow');
       return false;
     });
